0b2e26fe91d5de14bdbab77e57a55b73918f2bb8
VenenoFSD <venenofsd@qq.com>
2019-01-21 23:36:44 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+  import {get} from './util'
+  export default {
+    name: "App",
+    async created () {
+      const res = await get('/weapp/test');
+      console.log(res);
+    }
+  };
+</script>
+
+<style scoped>
+</style>
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const host = 'https://pxurh40h.qcloud.la';
+
+const config = {
+  host
+};
+
+export default config
