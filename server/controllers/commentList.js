82e603522f1883b5f6076d552f59f4f2d002828e
VenenoFSD <venenofsd@qq.com>
2019-01-27 23:46:46 +0800

    comment list interface update

--- server/controllers/commentList.js
+++ server/controllers/commentList.js
@@ -1,8 +1,14 @@
 const {mysql} = require('../qcloud');
 
 module.exports = async ctx => {
-  const {bookid} = ctx.request.query;
-  const commentList = await mysql('comments').select('comments.*', 'cSessionInfo.user_info').join('cSessionInfo', 'comments.openid', 'cSessionInfo.open_id').where('bookid', bookid);
+  const {bookid, openid} = ctx.request.query;
+  const mysqlSelect = mysql('comments').select('comments.*', 'cSessionInfo.user_info').join('cSessionInfo', 'comments.openid', 'cSessionInfo.open_id');
+  let commentList = [];
+  if (bookid) {
+    commentList = await mysqlSelect.where('bookid', bookid);
+  } else if (openid) {
+    commentList = await mysqlSelect.where('openid', openid);
+  }
   ctx.state.data = {
     list: commentList.map(v => {
       v.user_info = JSON.parse(v.user_info);
