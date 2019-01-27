643bd07e8fb560eb208791081cbf4dc1cf781461
VenenoFSD <venenofsd@qq.com>
2019-01-27 21:21:22 +0800

    book detail interface update

--- server/controllers/bookDetail.js
+++ server/controllers/bookDetail.js
@@ -2,5 +2,6 @@ const {mysql} = require('../qcloud');
 
 module.exports = async ctx => {
   const {id} = ctx.request.query;
+  ctx.state.data = await mysql('books').select().where('id', id).first();
   await mysql('books').where('id', id).increment('count', 1);
 };
